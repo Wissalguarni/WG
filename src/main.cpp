62ca187104d0ca31fe667038aaec258e3a357c98
Wissalguarni <guarni@insa-toulouse.fr>
2026-01-08 22:48:13 +0100

    test des methodes LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
 #include <Arduino.h>
 
 #include <Arduino.h>
-#include "Hardware\GroveLCD.hpp"
+#include "Hardware/GroveLCD.hpp"
 // TEST OF METHODS IN CLASS GroveLCD
-GroveLCD lcd;
+GroveLCD myLCD;  // Create an instance of your LCD class
 
 void setup() {
-    lcd.init();
+    Serial.begin(115200);           // For debugging
+    myLCD.init();                  // Initialize the LCD (if you have a begin/init method)
+    Serial.println("LCD Test Starting");
+   myLCD.setBacklight(255, 255, 255); // White
+    delay(500);
 
-    lcd.setCursor(0, 0); 
-    lcd.print("Hello");
+    // Test displaying scores
+    myLCD.displayScore(3, 5);
+    delay(2000);
 
-    lcd.setCursor(0, 1);
-    lcd.print("TEST COMPLETE");
+    // Test displaying game over screen
+    myLCD.displayGameOver(3, 5);
+    delay(2000);
+
+    // Test displaying winner
+    myLCD.displayVainqueur("Player 1");
+    delay(2000);
 }
 
 void loop() {
+    // Nothing needed in loop for a simple LCD test
 }
