a71dc4b844eb1bfdc09dcd0531ba87a27cdf9c0a
l-artiste-fg <goyheneix@insa-toulouse.fr>
2026-01-09 08:20:16 +0100

    correction de la structure et des include

--- src/MultiplayerGame.cpp
+++ src/MultiplayerGame.cpp
@@ -1,11 +1,3 @@
 #include "Game/MultiplayerGame.hpp"
 #include <Arduino.h>
 
-#define Player1_BUTTON 1
-#define Player2_BUTTON 2
-#define LED_PIN 3
-
-MultiplayerGame::MultiplayerGame(){
-     ledON=false;
-     gamefinished=false;
-};
--- src/button.cpp
+++ src/button.cpp
@@ -1,37 +1,3 @@
 #include "Hardware/Button.hpp"
 
-Button::Button(int buttonPin, GroveLCD* lcdScreen) 
-    : pin(buttonPin), lastState(false), lcd(lcdScreen) {}
 
-void Button::init() {
-    pinMode(pin, INPUT_PULLUP); //si appuyé = LOW
-    lastState = this->isPressed();
-
-    if (lcd) {
-        lcd->clear();
-        lcd->setCursor(0, 0);
-        lcd->print("Appuyez sur le");
-        lcd->setCursor(0, 1);
-        lcd->print("bouton !");
-    }while (!isPressed()) {
-        delay(10);
-    }
-
-    if (lcd) {
-        lcd->clear();
-        lcd->setCursor(0, 0);
-        lcd->print("Merci !");
-    }
-    delay(1000);
-}
-
-bool Button::isPressed() {
-    bool currentState = digitalRead(pin);
-
-    if (lastState == HIGH && currentState == LOW) {
-        lastState = currentState;
-        delay(50); // anti-rebond simple
-        return true;
-    }
-    return false;
-}
